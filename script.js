f8bbf3bd00dd3b1673700bfb4b60a981843d1ba2
Mostafa-Mohamed-Atef <mostafamohamedatef27@gmail.com>
2024-10-02 23:23:54 +0300

    added userInput var

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
+//start chat
 var i = 0;
 var j = 0;
-var txt = ['<?> what is black and blue and DEAD all over?', "<?> YOU"];
+var txt = ["<?> Haven't a clue?", "<?> let's play a game", "<?> just me and you"];
 var speed = 100;
 
-// prompt = getElementById("batmanTalking")
-
-
-function typeWriter() {
+function startChat() {
   if (j < txt.length) {
     if (i < txt[j].length) {
-      document.getElementById("message").innerHTML += txt[j].charAt(i);
+      document.getElementById("startMessage").innerHTML +=  txt[j].charAt(i);
       i++;
-      setTimeout(typeWriter, speed);
+      setTimeout(startChat, speed);
     } else {
-      document.getElementById("message").innerHTML += "<br>";
+      document.getElementById("startMessage").innerHTML += "<br>";
       j++;
       i = 0;
-      speed = 500;
-      setTimeout(typeWriter, speed);
+      setTimeout(startChat, speed);
     }
   }
 }
-typeWriter();
+startChat(); 
+document.getElementById('batmanTalks').addEventListener('change', function() {
+  var userInput = this.value; // Assign input value to variable
+  console.log(userInput);
+});
+
